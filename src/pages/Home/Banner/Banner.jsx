f14e068ffa13929307e6bfd20d7d909e91bc71c4
Iqubal905 <iqubal905@gmail.com>
2023-05-18 02:13:54 +0600

    banner add

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
-
+import img1 from '../../../assets/c-1.jpg'
+import img2 from '../../../assets/c-2.jpg'
+import img3 from '../../../assets/c-3.jpg'
 const Banner = () => {
     return (
-        <div>
-            <h2>Banner</h2>
+        <div className=' '>
+          <div className="carousel w-full max-h-screen">
+  <div id="slide1" className="carousel-item relative w-full ">
+    <img src={img1} className="w-full " />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src={img2}className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src={img3} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src={img3} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
--- src/pages/Shared/NabBar/Navbar.jsx
+++ src/pages/Shared/NabBar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-           <div className="navbar bg-base-100">
+           <div className="navbar  bg-indigo-100 fixed top-0 left-0 right-0 z-50">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
